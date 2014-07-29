49d2c96ae7f2dcba91da15e793bf53ed33496a1d
abc <abc@abcui-MacBook-Air.local>
2014-07-29 16:12:16 +0900

    init

---
+++ cdb_crashdetect.py
@@ -0,0 +1,165 @@
+#
+#   Simple CDB(console windbg) crashdetector
+#
+#       chpie@grayhash
+#       License : BSD
+#
+#
+#   Dependency
+#       windbg
+#       MSEC exploitable
+#
+
+import os
+import re
+import sys
+import time
+import threading
+import subprocess
+
+################################ config
+
+timeout_seconds = 60
+
+sympath = "SRV*c:\\code\\symbols*http://msdl.microsoft.com/download/symbols;SRV*c:\\code\\symbols*http://chromium-browser-symsrv.commondatastorage.googleapis.com;"
+CDB_PATH = 'cdb.exe -y "%s" -G -o -cfr script.txt' % sympath
+
+#################################
+
+timer = None
+crash_report_cmd = '!exploitable;.lastevent;r;u;q'
+events = ['asrt', 'av', 'dm', 'dz', 'c000008e', 'gp', 'ii', 'iov', 'ip', 'isc', 'lsq', 'sbo', 'sov', 'chhc', 'ssessec', 'bpebpec', '80000003']
+
+hoho = []
+for ev in events:
+    hoho.append('sx- -c "%s" %s\n' % (crash_report_cmd, ev))
+
+script = \
+'''
+.logopen "%s\\cdb_log.txt"
+.load msec
+%s
+sxi ibp
+.pcmd -s "gn"
+g
+''' % (os.getcwd(), ''.join(hoho))
+
+def kill_cdb(popo):
+    global timer
+    try: popo.terminate()
+    except: pass
+    try: timer.cancel()
+    except: pass
+
+def test(cmd):
+    global timeout_seconds
+    global timer
+
+    f = open('script.txt', 'wb+')
+    f.write(script)
+    f.close()
+
+    #
+    # Create new process group for COOL display
+    #
+    p = subprocess.Popen(args=cmd, 
+            creationflags=subprocess.CREATE_NEW_PROCESS_GROUP | subprocess.CREATE_NEW_CONSOLE)
+
+    timer = threading.Timer(timeout_seconds, kill_cdb, [p])
+    timer.start()
+    p.wait()
+    try: timer.cancel()
+    except: pass
+
+def sx( regular_expression, data ):
+    ret = re.search(regular_expression, data)
+    if None != ret: ret = ret.group(1)
+    return ret.strip()
+
+def zzz(unik):
+    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()[]'
+    ret = ''
+    for ch in unik:
+        if ch not in charset: ch = '_'
+        ret += ch
+    while '__' in ret: ret = ret.replace('__', '_')
+
+    return ret
+
+
+def parser(data):
+    global browser
+
+    if 'Exploitability Classification:' not in data: return False
+
+    data = data[ data.find('Exploitability Classification') : ]
+
+    ev_class = sx("Exploitability Classification: (.+)[\r\n]*", data)
+    ev_title = sx("Recommended Bug Title: (.+)[\r\n]*", data)
+    ev_event = sx("Last event: [a-f0-9A-F]+\.[a-f0-9A-F]+: (.+) - code", data)
+    instruction = sx("[\r\n]+[a-f0-9A-F]+[ \t]+[a-f0-9A-F]+[ \t]+(.+)[\r\n]+", data)
+    at = sx("[\r\n]+(.+)[\r\n]+[a-f0-9A-F]+[ \t]+[a-f0-9A-F]+[ \t]+.+[\r\n]+", data)
+
+    if ev_class and ev_title and ev_event and instruction:
+        while '  ' in instruction: 
+            instruction = instruction.replace('  ', ' ')
+
+        unik = '_'.join([browser, ev_class, ev_event, at, instruction])
+
+        unik = zzz(unik)
+
+        ##
+        #
+        #   You can capture crash information using those strings below in your wrapper launcher script
+        #
+        ##
+        print '[METAINFO_BEGIN]'
+        print data
+        print '[METAINFO_END]'
+        print '[CAPTURE_BEGIN]'
+        print unik
+        print '[CAPTURE_END]'
+
+        return True
+
+    return False
+
+def wait_for_process_die(die):
+    while True:
+        p = subprocess.Popen('tasklist', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        so, se = p.communicate()
+        if die not in so: break
+        time.sleep(1)
+
+##############################################################
+
+if len(sys.argv) != 4:
+    ##
+    #
+    #   0 : self
+    #   1 : browser (only used for display information)
+    #   2 : target application
+    #   3 : url
+    #
+    print 'Invalid arguments %s != 4' % len(sys.argv)
+    quit()
+
+browser = sys.argv[1]
+cmd = sys.argv[2]
+url = sys.argv[3]
+
+die = cmd[ cmd.rfind('\\') + 1 : cmd.rfind('.exe') + 4 ]
+
+test(CDB_PATH + ' ' + cmd + ' ' + url)
+wait_for_process_die(die)
+if parser(open('cdb_log.txt', 'rb').read()) == True:
+    os.system('tskill werfault')
+
+
+
+
+
+
+
+
+
